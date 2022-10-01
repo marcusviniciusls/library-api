895a858b6ebd0b0337c4bb91be98cde1fd6dcb47
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-09-30 22:08:13 -0300

    Documentacao de API

--- src/main/java/com/marcus/silva/dev/libraryapi/controller/BookController.java
+++ src/main/java/com/marcus/silva/dev/libraryapi/controller/BookController.java
@@ -5,6 +5,8 @@ import com.marcus.silva.dev.libraryapi.dto.request.BookUpdateForm;
 import com.marcus.silva.dev.libraryapi.dto.response.BookResponse;
 import com.marcus.silva.dev.libraryapi.dto.response.BookReturnResponse;
 import com.marcus.silva.dev.libraryapi.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,41 +19,48 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@Api("Book API")
 public class BookController {
 
     @Autowired private BookService bookService;
 
     @PostMapping
+    @ApiOperation("Criar um livro")
     public ResponseEntity<BookResponse> create(@Valid @RequestBody BookSaveForm bookSaveForm){
         BookResponse bookResponse = bookService.saveBook(bookSaveForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookResponse);
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiOperation("Apagar um livro por Id")
     public ResponseEntity<?> deleteById(@PathVariable Long id){
         bookService.deleteById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PutMapping(value = "/{id}")
+    @ApiOperation("Deletar um livro por Id")
     public ResponseEntity<BookResponse> deleteById(@PathVariable Long id, @RequestBody BookUpdateForm bookUpdateForm){
         BookResponse bookResponse = bookService.refreshById(id, bookUpdateForm);
         return ResponseEntity.ok(bookResponse);
     }
 
     @GetMapping(value = "/{id}")
+    @ApiOperation("Buscar um livro por Id")
     public ResponseEntity<BookResponse> findById(@PathVariable Long id){
         BookResponse bookResponse = bookService.findByIdBook(id);
         return ResponseEntity.ok(bookResponse);
     }
 
     @GetMapping
+    @ApiOperation("Buscar todos os livros")
     public ResponseEntity<Page<BookResponse>> findAll(Pageable pageable){
         Page<BookResponse> pageBookResponse = bookService.findAllBook(pageable);
         return ResponseEntity.ok(pageBookResponse);
     }
 
     @GetMapping(value = "/return")
+    @ApiOperation("Retornar os livros com prazos de entrega atrasados ou que vai vencer em 10 dias")
     public ResponseEntity<List<BookReturnResponse>> returnBook(){
         List<BookReturnResponse> bookReturnResponses = bookService.bookLaterAndLimitTenDays();
         return ResponseEntity.ok(bookReturnResponses);
--- src/main/java/com/marcus/silva/dev/libraryapi/controller/LoanController.java
+++ src/main/java/com/marcus/silva/dev/libraryapi/controller/LoanController.java
@@ -4,10 +4,11 @@ package com.marcus.silva.dev.libraryapi.controller;
 import com.marcus.silva.dev.libraryapi.dto.request.LoanReturnSave;
 import com.marcus.silva.dev.libraryapi.dto.request.LoanSaveForm;
 
-import com.marcus.silva.dev.libraryapi.dto.response.BookResponse;
 import com.marcus.silva.dev.libraryapi.dto.response.BookResponsePage;
 import com.marcus.silva.dev.libraryapi.dto.response.LoanResponse;
 import com.marcus.silva.dev.libraryapi.service.LoanService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/loan")
+@Api("Loan API")
 public class LoanController {
 
 
@@ -24,24 +26,28 @@ public class LoanController {
     private LoanService loanService;
 
     @PostMapping
+    @ApiOperation("Criar um emprestimo do livro, caso esteja disponível")
     public ResponseEntity<LoanResponse> create(@Valid @RequestBody LoanSaveForm loanSaveForm){
         LoanResponse loanResponse = loanService.saveLoan(loanSaveForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(loanResponse);
     }
 
     @PostMapping(value = "/return")
+    @ApiOperation("Devolver um Livro")
     public ResponseEntity<?> returnLoan(@Valid @RequestBody LoanReturnSave loanReturnSave){
         loanService.loanReturn(loanReturnSave);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping(value = "/{id}")
+    @ApiOperation("Buscar um emprestimo por Id")
     public ResponseEntity<LoanResponse> findById(@PathVariable Long id){
         LoanResponse loanResponse = loanService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(loanResponse);
     }
 
     @GetMapping(value = "/book/{id}")
+    @ApiOperation("Buscar um livro por Id")
     public ResponseEntity<BookResponsePage> findAllLoanPerBook(@PathVariable Long id){
         BookResponsePage bookResponsePage = loanService.findAllLoanResponse(id);
         return ResponseEntity.ok(bookResponsePage);
